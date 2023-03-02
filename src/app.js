59a38a0b78e39485339caede809c8e7706ee36a5
Davi <daviribeirotesta@gmail.com>
2023-03-02 16:56:37 -0300

    first commit: validateSchema and server configuration

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express, {json} from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(json());
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
